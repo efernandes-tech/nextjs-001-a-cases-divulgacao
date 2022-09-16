f2f89de4b505019b066bcba3e80c9e859b69cba5
Éderson Fernandes <ederson.fernandes@bempromotora.onmicrosoft.com>
2022-09-16 18:11:38 -0300

    getStaticProps

--- README.md
+++ README.md
@@ -26,3 +26,7 @@ cd ./out
 ```
 npx http-server ./out -c-1
 ```
+
+## API Questions:
+
+https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json
--- pages/faq.js
+++ pages/faq.js
@@ -1,12 +1,49 @@
+import { useEffect, useState } from 'react';
 import Link from '../src/components/Link';
 
-export default function FaqPage() {
+export async function getStaticProps() {
+    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+    const faq = await fetch(FAQ_API_URL)
+        .then((response) => response.json())
+        .then((json) => {
+            return setFaq(json);
+        })
+    return {
+        props: {
+            faq
+        },
+    }
+}
+
+export default function FaqPage({ faq }) {
+    // const [faq, setFaq] = useState([]);
+    // useEffect(() => {
+    // }, [])
+
     return (
         <div>
             <h1>Alura Cases - FAQ</h1>
             <Link href="/">
                 Ir para a Home
             </Link>
+            <ul>
+                {
+                    faq.map(({ answer, question }) => {
+                        return (
+                            <li key={answer}>
+                                <article>
+                                    <h2>
+                                        {question}
+                                    </h2>
+                                    <p>
+                                        {answer}
+                                    </p>
+                                </article>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
         </div>
     );
 }
