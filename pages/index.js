98e1ece9da16576efda7cdda56ad58108770c8c4
Éderson Fernandes <ederson.fernandes@bempromotora.onmicrosoft.com>
2022-09-16 17:20:08 -0300

    Componentes do layout

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,36 @@
-import Link from '../src/components/Link';
+import HomeScreen from "../src/screens/HomeScreen";
 
-function Title({ children, as }) {
-    const Tag = as;
-    return (
-        <>
-            <Tag>
-                {children}
-            </Tag>
-            <style jsx>{`
-                ${Tag} {
-                    color: red;
-                }
-            `}</style>
-        </>
-    )
-}
+export default HomeScreen;
 
-export default function HomePage() {
-    return (
-        <div>
-            <Title as="h1">
-                Alura Cases - Home Page
-            </Title>
-            <Link href="/faq">
-                Ir para a página do FAQ
-            </Link>
-        </div>
-    );
-}
+// import Link from '../src/components/Link';
+// import Footer from '../src/components/patterns/Footer';
+
+// function Title({ children, as }) {
+//     const Tag = as;
+//     return (
+//         <>
+//             <Tag>
+//                 {children}
+//             </Tag>
+//             <style jsx>{`
+//                 ${Tag} {
+//                     color: red;
+//                 }
+//             `}</style>
+//         </>
+//     )
+// }
+
+// export default function HomePage() {
+//     return (
+//         <div>
+//             <Title as="h1">
+//                 Alura Cases - Home Page
+//             </Title>
+//             <Link href="/faq">
+//                 Ir para a página do FAQ
+//             </Link>
+//             <Footer />
+//         </div>
+//     );
+// }
--- src/components/patterns/Footer/index.js
+++ src/components/patterns/Footer/index.js
@@ -33,7 +33,7 @@ export default function Footer() {
                         color: theme.colors.neutral[400],
                     }}
                 >
-                    &copy; {new Date().getFullYear()} DevSoutinho. Todos os direitos reservados.
+                    &copy; {new Date().getFullYear()} @edersonlrf. Todos os direitos reservados.
                 </Text>
             </Box>
         </Box>
