093e684dde03ac07f14bd4c94fcdb992b03afcc8
Éderson Fernandes <edersonluis.rf@gmail.com>
2022-09-17 10:40:20 -0300

    Preparando faq.js e FAQScreen

--- pages/faq.js
+++ pages/faq.js
@@ -1,60 +1,24 @@
-import PageTitle from '../src/components/PageTitle';
+// faq.js
+import FAQScreen from '../src/screens/FAQScreen';
 
-import Link from '../src/components/Link';
+export default FAQScreen;
 
-// SSG - Static Site Generation
-// SSR - Server Side Rendering
-// ISG - Incremental Static Generation
-
-/*
-export async function getServerSideProps() {
-    console.log('getStaticProps() - roda somente no build');
-    console.log('getServerSideProps() - atencao, em modo DEV sempre roda a cada acesso');
-*/
-
-export async function getServerSideProps() {
-    console.log('getServerSideProps() - roda a cada acesso recebido em PROD');
-    console.log('getServerSideProps() - atencao, em modo DEV sempre roda a cada acesso');
-
+export async function getStaticProps() {
+    console.log('Em modo DEV, sempre roda! A cada acesso')
+    console.log('Roda SOMENTE em build time')
     const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
     const faq = await fetch(FAQ_API_URL)
-        .then((response) => response.json())
-        .then((json) => {
-            return json;
+        .then((respostaDoServidor) => {
+            return respostaDoServidor.json();
         })
+        .then((resposta) => {
+            return resposta;
+        });
+
     return {
         props: {
-            faq
+            qualquercoisa: 'que eu passar aqui',
+            faq,
         },
-    }
-}
-
-export default function FaqPage({ faq }) {
-    return (
-        <div>
-            <PageTitle>FAQ - Alura Cases Campanha</PageTitle>
-            <h1>Alura Cases - FAQ</h1>
-            <Link href="/">
-                Ir para a Home
-            </Link>
-            <ul>
-                {
-                    faq.map(({ answer, question }) => {
-                        return (
-                            <li key={answer}>
-                                <article>
-                                    <h2>
-                                        {question}
-                                    </h2>
-                                    <p>
-                                        {answer}
-                                    </p>
-                                </article>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
-        </div>
-    );
+    };
 }
